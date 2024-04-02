c6c5a3d87df49d684ed45c648bc1ca170806684e
JacobKrueger42 <128105560+JacobKrueger42@users.noreply.github.com>
2024-04-02 21:12:15 +1100

    landing page func

---
+++ frontend/src/pages/Landing/landing.jsx
@@ -0,0 +1,60 @@
+import { AppBar, Toolbar, CardActions } from '@mui/material';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import Typography from '@mui/material/Typography';
+import { useEffect } from 'react';
+import MenuAppBar from '../../components/MenuAppBar';
+import anonBanner from '../../assets/images/bannerPlaceholder.jpg';
+import { useNavigate } from 'react-router-dom';
+
+export default function AnonPage() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+
+    }, []);
+
+    const handleShopNow = () => {
+        navigate('/shop'); 
+    };
+
+    return (
+        <>
+            <MenuAppBar />
+            <div style={{ 
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'stretch',
+                height: '40vh',
+                justifyContent: 'space-between',
+            }}>
+                <Card sx={{ flexGrow: 1, width: 'calc(50% - 10px)' }} variant='outlined'>
+                    <CardHeader title='Explore IoT Bay' />
+                    <CardContent>
+                        <Typography variant='h5' component="div">
+                            Explore the Future of Technology
+                        </Typography>
+                        <Typography variant='body2' color="text.secondary">
+                            Discover how our cutting-edge IoT solutions can transform your daily life.
+                        </Typography>
+                        </CardContent>
+                    <CardActions style={{ justifyContent: 'center' }}>
+                        <Button variant="contained" onClick={handleShopNow}>Shop Now</Button>
+                    </CardActions>
+                </Card>
+                <img 
+                    src={anonBanner} 
+                    alt="Anon Page Banner" 
+                    style={{ 
+                        flexGrow: 1,
+                        width: '50%',
+                        height: '100%',
+                        objectFit: 'cover',
+                    }} 
+                />
+            </div>
+        </>
+    );
+}
