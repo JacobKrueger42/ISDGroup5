45320f126836b7142b2ca8418eacbcddabb845a1
Albert Ferguson <albertferguson118@gmail.com>
2024-04-10 00:29:01 +1000

    middleware handling fixed

--- backend/src/middleware/RequireRole.js
+++ backend/src/middleware/RequireRole.js
@@ -9,11 +9,13 @@ export default async function requireRole(req, res, next, callback, roles) {
     if (typeof callback !== 'function') {
         console.error('a valid callback must be supplied to this middleware');
         next(new Error('Authorisation invalid'));
+        return;
     }
 
-    if (!roles || roles.length > 0) {
+    if (!roles || roles.length === 0) {
         console.error('a role must be supplied to this middleware');
         next(new Error('Authorisation invalid'));
+        return;
     }
 
     await requireAuth(req, res, next, async () => {
@@ -21,30 +23,22 @@ export default async function requireRole(req, res, next, callback, roles) {
         const user = await getUserAsync(req.session.userId);
 
         // check which provided roles intersect with available roles
-        const inter = availableRoles.filter(availRole =>
-            roles.includes(availRole)
-        );
-
-        if (inter.length > 0) {
+        if (
+            availableRoles.filter(availRole => roles.includes(availRole))
+                .length === 0
+        ) {
             console.error(
                 `none of the provided roles are available. Provided: "${roles}", available "${availableRoles}"`
             );
             next(new Error('Authorisation invalid'));
-        }
-
-        if (!roles.includes(user.role)) {
+        } else if (!roles.includes(user.role)) {
             const forbiddenMessage = `Invalid role, cannot access "${req.path}"`;
             console.log(forbiddenMessage);
-
-            res.header('Content-Type', 'application/json')
-                .status(HttpStatus.FORBIDDEN)
-                .json({
-                    path: req.path,
-                    detailed_error_message: forbiddenMessage,
-                    message: 'Forbidden'
-                });
+            next(new Error(forbiddenMessage));
+        } else {
+            console.log('callback triggered');
+            await callback();
+            next();
         }
-
-        await callback();
     });
 }
