d9ace407f8c35eaf41b5a8cae253356e54f765cb
Albert Ferguson <albertferguson118@gmail.com>
2024-04-11 00:18:20 +1000

    grouping and loading checks

--- frontend/src/components/SearchInput.jsx
+++ frontend/src/components/SearchInput.jsx
@@ -3,8 +3,13 @@ import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import Autocomplete from '@mui/material/Autocomplete';
 
+// currently bugged and sorts in reverse idky
 // search input allowing free text input but suggests options
+// will group by category if it exists, else fallback to alphabetical order
 export default function SearchInput({ options }) {
+    const hasCategory =
+        options?.length > 0 && Object.keys(options[0]).includes('category');
+
     return (
         <Stack spacing={4} direction='row' alignItems='center'>
             <Typography align='left' variant='body' color='text.secondary'>
@@ -16,6 +21,9 @@ export default function SearchInput({ options }) {
                 id='free-solo-2-demo'
                 disableClearable
                 options={options.map(option => option.name)}
+                groupBy={option =>
+                    hasCategory ? option.category : option[0].toUpperCase()
+                }
                 sx={{ flexGrow: 1 }}
                 renderInput={params => (
                     <TextField
