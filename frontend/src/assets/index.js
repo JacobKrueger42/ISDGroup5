45003ca0405d14edaf9ef408a9a2d960204b0b18
Albert Ferguson <albertferguson118@gmail.com>
2024-05-09 22:52:38 +1000

    reference

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,4 +1,6 @@
 import bannerPlaceholder from '../assets/bannerPlaceholder.jpg';
+
+// Risk of Rain 2 items are a fun theme here, see: https://riskofrain2.fandom.com/wiki/Items
 import LaserScopeUrl from '../assets/demoProducts/Laser_Scope.png';
 import DefensiveMicrobotsUrl from '../assets/demoProducts/Defensive_Microbots.png';
 import HeadStompersUrl from '../assets/demoProducts/H3AD-5T_v2.png';
