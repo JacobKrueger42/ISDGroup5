c5a92adcf02f8288cb41d7691e1da78ffde7e48f
Albert Ferguson <albertferguson118@gmail.com>
2024-05-09 23:38:54 +1000

    external link

--- frontend/src/pages/Catalogue/CatalogueEntryCard.jsx
+++ frontend/src/pages/Catalogue/CatalogueEntryCard.jsx
@@ -5,6 +5,7 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
+// import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 
 export default function CatalogueEntryCard({ item, assetFn }) {
@@ -12,7 +13,7 @@ export default function CatalogueEntryCard({ item, assetFn }) {
     const isDisabled = false;
 
     return (
-        <Grid item sm='2' md='5' lg='4'>
+        <Grid item sm={2} md={5} lg={4}>
             <Card sx={{ maxWidth: 345 }}>
                 <CardMedia
                     sx={{ height: 140, backgroundSize: 'contain' }}
@@ -40,7 +41,14 @@ export default function CatalogueEntryCard({ item, assetFn }) {
                     >
                         Add to cart
                     </Button>
-                    <Button size='small'>{item.brandName}</Button>
+                    <Button
+                        size='small'
+                        target='_blank'
+                        component='a'
+                        href='https://riskofrain2.fandom.com/wiki/Risk_of_Rain_2_Wiki'
+                    >
+                        {item.brandName}
+                    </Button>
                 </CardActions>
             </Card>
         </Grid>
