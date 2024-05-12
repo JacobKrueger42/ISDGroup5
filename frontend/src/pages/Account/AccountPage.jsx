16994e2295b5839b2de2a7b76d2b07e6f5c3b5f8
Albert Ferguson <albertferguson118@gmail.com>
2024-05-12 21:02:19 +1000

    account page retrofitted

--- frontend/src/pages/Account/AccountPage.jsx
+++ frontend/src/pages/Account/AccountPage.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react';
+import { useAuth } from '#hooks';
 import {
+    Button,
     Card,
     CardContent,
-    Typography,
-    TextField,
-    Button,
     Dialog,
     DialogActions,
     DialogContent,
-    DialogTitle
+    DialogTitle,
+    TextField,
+    Typography
 } from '@mui/material';
-import { useAuth } from '#hooks';
+import { Layout } from '#components';
+import { useState } from 'react';
 import UpdateAccount from './UpdateAccount';
 
 export default function AccountPage() {
@@ -65,103 +66,107 @@ export default function AccountPage() {
         }
     };
 
-    if (user) {
-        return (
-            <>
-                <Card
-                    sx={{
-                        textAlign: 'left',
-                        maxWidth: '550px'
-                    }}
-                >
-                    <CardContent
-                        sx={{
-                            textAlign: 'left',
-                            marginBottom: '10px'
-                        }}
-                    >
-                        <Typography variant='h5' gutterBottom>
-                            Account Information
-                        </Typography>
-                        <TextField
-                            label='First Name'
-                            value={user.firstName}
-                            fullWidth
-                            margin='normal'
-                            disabled
-                        />
-                        <TextField
-                            label='Last Name'
-                            value={user.lastName}
-                            fullWidth
-                            margin='normal'
-                            disabled
-                        />
-                        <TextField
-                            label='Email'
-                            value={user.email}
-                            fullWidth
-                            margin='normal'
-                            disabled
-                        />
-                        <TextField
-                            label='Phone Number'
-                            value={user.phone}
-                            fullWidth
-                            margin='normal'
-                            disabled
-                        />
-                    </CardContent>
-                    <CardContent>
-                        <Button
-                            sx={{
-                                marginRight: '10px'
-                            }}
-                            variant='contained'
-                            color='primary'
-                            onClick={handleUpdateButtonClick}
-                        >
-                            Update Information
-                        </Button>
-                        <Button
-                            variant='contained'
-                            color='error'
-                            onClick={handleDeleteButtonClick}
-                        >
-                            Delete Account
-                        </Button>
-                    </CardContent>
-                </Card>
+    if (!user) return <></>;
 
-                <UpdateAccount
-                    open={openUpdateAccount}
-                    onClose={onCloseUpdateAccount}
-                    error={error}
-                    user={user}
-                    onSubmit={handleSubmit}
-                />
-                <Dialog
-                    open={openDeleteConfirmation}
-                    onClose={onCloseDeleteConfirmation}
-                >
-                    <DialogTitle>Delete Account</DialogTitle>
-                    <DialogContent>
-                        Are you sure you want to delete your account?
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={onCloseDeleteConfirmation}>
-                            Cancel
-                        </Button>
-                        <Button
-                            variant='contained'
-                            color='error'
-                            onClick={handleDeleteConfirm}
-                        >
-                            Yes
-                        </Button>
-                    </DialogActions>
-                </Dialog>
-            </>
-        );
-    }
+    return (
+        <Layout
+            title='My Account'
+            headerContent={
+                <Typography variant='h5' gutterBottom>
+                    Account Information
+                </Typography>
+            }
+            headerActions={
+                <>
+                    <UpdateAccount
+                        open={openUpdateAccount}
+                        onClose={onCloseUpdateAccount}
+                        error={error}
+                        user={user}
+                        onSubmit={handleSubmit}
+                    />
+                    <Dialog
+                        open={openDeleteConfirmation}
+                        onClose={onCloseDeleteConfirmation}
+                    >
+                        <DialogTitle>Delete Account</DialogTitle>
+                        <DialogContent>
+                            Are you sure you want to delete your account?
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={onCloseDeleteConfirmation}>
+                                Cancel
+                            </Button>
+                            <Button
+                                variant='contained'
+                                color='error'
+                                onClick={handleDeleteConfirm}
+                            >
+                                Yes
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
+
+                    <Button
+                        sx={{
+                            marginRight: '10px'
+                        }}
+                        variant='contained'
+                        color='primary'
+                        onClick={handleUpdateButtonClick}
+                    >
+                        Update Information
+                    </Button>
+                    <Button
+                        variant='contained'
+                        color='error'
+                        onClick={handleDeleteButtonClick}
+                    >
+                        Delete Account
+                    </Button>
+                </>
+            }
+        >
+            <Card>
+                <CardContent>
+                    <TextField
+                        label='First Name'
+                        value={user.firstName}
+                        fullWidth
+                        margin='normal'
+                        InputProps={{
+                            readOnly: true
+                        }}
+                    />
+                    <TextField
+                        label='Last Name'
+                        value={user.lastName}
+                        fullWidth
+                        margin='normal'
+                        InputProps={{
+                            readOnly: true
+                        }}
+                    />
+                    <TextField
+                        label='Email'
+                        value={user.email}
+                        fullWidth
+                        margin='normal'
+                        InputProps={{
+                            readOnly: true
+                        }}
+                    />
+                    <TextField
+                        label='Phone Number'
+                        value={user.phone}
+                        fullWidth
+                        margin='normal'
+                        InputProps={{
+                            readOnly: true
+                        }}
+                    />
+                </CardContent>
+            </Card>
+        </Layout>
+    );
 }
