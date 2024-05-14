c9d5f490822afcb1aad21bee98db95f2848f6d73
Albert Ferguson <albertferguson118@gmail.com>
2024-05-14 20:05:30 +1000

    setup the transponder client

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -1,6 +1,17 @@
 import { default as prisma } from '../services/DatabaseClient.js';
+import { default as transponder } from '../services/TransponderClient.js';
+
+import { default as emailNotifier } from '../services/EmailNotifier.js';
+
 import { default as userAuthRepository } from '../services/UserAuthRepository.js';
 import { default as productRepository } from '../services/ProductRepository.js';
 import { default as catalogueRepository } from '../services/CatalogueRepository.js';
 
-export { prisma, userAuthRepository, productRepository, catalogueRepository };
+export {
+    prisma,
+    userAuthRepository,
+    productRepository,
+    catalogueRepository,
+    transponder,
+    emailNotifier
+};
