8d009d0d04dcaf52b8493aae0743ff0c6dc6e739
Albert Ferguson <albertferguson118@gmail.com>
2024-05-14 23:48:38 +1000

    fixup dropdown

--- frontend/src/components/Dropdown.jsx
+++ frontend/src/components/Dropdown.jsx
@@ -1,24 +1,17 @@
-import { Select, MenuItem } from '@mui/material';
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 
-export default function Dropdown({ options, ...dropDownProps }) {
+export default function Dropdown({ options, error, ...dropDownProps }) {
     return (
-        <Select
-            displayEmpty
-            {...dropDownProps}
-            renderValue={selected =>
-                selected === '' ? dropDownProps.placeholder : selected
-            }
-        >
-            <MenuItem disabled value=''>
-                <em>{dropDownProps.placeholder ?? 'Placeholder'}</em>
-            </MenuItem>
-
-            {options &&
-                options.map((option, index) => (
-                    <MenuItem value={option} key={index}>
-                        {option}
-                    </MenuItem>
-                ))}
-        </Select>
+        <FormControl fullWidth required error={!!error} margin='normal'>
+            <InputLabel id={dropDownProps.id}>{dropDownProps.label}</InputLabel>
+            <Select displayEmpty {...dropDownProps}>
+                {options &&
+                    options.map((option, index) => (
+                        <MenuItem value={option} key={index}>
+                            {option}
+                        </MenuItem>
+                    ))}
+            </Select>
+        </FormControl>
     );
 }
